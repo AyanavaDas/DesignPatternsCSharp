8c303d2e9b8fdf61cd89d111a2ddaa4ee8025e1b
AyanavaDas <51167125+AyanavaDas@users.noreply.github.com>
2022-12-05 18:57:40 +0530

    added model test case

---
+++ csharpcore/GildedRoseTests/ItemTest.cs
@@ -0,0 +1,19 @@
+﻿using GildedRoseKata;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GildedRoseTests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void ItemModelTest()
+        {
+            var items = new Item();
+
+            Assert.Equal(null, items.Name); 
+            Assert.Equal(0, items.SellIn);
+            Assert.Equal(0, items.Quality);
+        }
+    }
+}
